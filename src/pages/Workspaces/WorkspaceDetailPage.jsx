f463d7e85750bc58f71c8a3b3748ad167cc2089f
frendikapratama <frendikapratama28@gmail.com>
2025-12-16 08:56:16 +0700

    filtering departemen send requase collaboration

--- src/pages/Workspaces/WorkspaceDetailPage.jsx
+++ src/pages/Workspaces/WorkspaceDetailPage.jsx
@@ -263,7 +263,7 @@ const WorkspaceDetailPage = () => {
             </button>
           </div>
         </div>
-        <CollaborationTab workspaceId={id} />
+        <CollaborationTab workspaceId={id} currentKuarterId={data?.kuarter} />
       </div>
       {isMember && user && token && (
         <div
