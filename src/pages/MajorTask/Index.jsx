88d3af197740b3b7fc11db4968b8009758307a9e
frendikapratama <frendikapratama28@gmail.com>
2026-02-09 10:36:41 +0700

    fix

--- src/pages/MajorTask/Index.jsx
+++ src/pages/MajorTask/Index.jsx
@@ -390,7 +390,7 @@ const MajorTaskPage = () => {
                             )}
 
                             {/* Tooltip */}
-                            <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 px-2 py-1 bg-gray-900 text-white text-xs rounded opacity-0 group-hover:opacity-100 transition-all duration-200 pointer-events-none whitespace-nowrap z-[9999]">
+                            <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 px-2 py-1 bg-gray-900 text-white text-xs rounded opacity-0 group-hover:opacity-100 transition-all duration-200 pointer-events-none whitespace-nowrap ">
                               <div className="font-medium">
                                 {picUser.username}
                               </div>
