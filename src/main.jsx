3354ad27b29ccc010648d1055da1d64f238740a2
frendikapratama <frendikapratama28@gmail.com>
2026-07-07 09:26:49 +0700

    0.1.31

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { Toaster } from "react-hot-toast";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
