c797597d4b4db54605a2b35c2c9673a52f251791
frendikapratama <frendikapratama28@gmail.com>
2026-07-08 21:58:19 +0700

    0.1.32

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,6 @@ import IndexRooms from "./pages/BookingMeeting/Rooms/Index";
 import IndexBooking from "./pages/BookingMeeting/MeetingManagement/Index";
 import IndexMySchedule from "./pages/BookingMeeting/MySchedule/Index";
 import MeetingRecapPage from "./pages/BookingMeeting/MeetingRecap/Index";
-
 export default function App() {
   if (import.meta.env.MODE === "development") {
     console.log = () => {};
